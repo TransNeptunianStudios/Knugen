84dbbe43ef5bb92e56b8bf3c4fea9a040f2444e8
Mikael Larsson <c.mikael.larsson@gmail.com>
2015-02-23 21:59:17 +0100

    Added some more temporary textures for Knugen

--- src/Kungen.js
+++ src/Kungen.js
@@ -1,5 +1,6 @@
 Kungen = function(game) {
-   Phaser.Sprite.call(this, game, game.world.width/2, game.world.height/2, 'kungen');
+
+   Phaser.Sprite.call(this, game, game.world.width/2, game.world.height/2, 'kungen0');
 
    game.physics.arcade.enable(this);
 
@@ -7,6 +8,8 @@ Kungen = function(game) {
    this.anchor.setTo(0.5);
    this.body.collideWorldBound = true;
    this.body.allowRotation = false;
+   this.currentImage = 0;
+   this.imageMap = { '0' : 'kungen0', '1' : 'kungen90', '2' : 'kungen180', '3' : 'kungen270' }
 }
 
 Kungen.prototype = Object.create(Phaser.Sprite.prototype);
@@ -14,12 +17,35 @@ Kungen.prototype.constructor = Kungen;
 
 Kungen.prototype.update = function() {
    if (this.game.input.activePointer.isDown) {
-
-      var angToP = this.game.physics.arcade.angleToPointer(this, this.game.input.activePointer);
-      this.body.velocity = this.game.physics.arcade.velocityFromRotation(angToP, 60);
+      var radToPointer = this.game.physics.arcade.angleToPointer(this, this.game.input.activePointer);
+      this.body.velocity = this.game.physics.arcade.velocityFromRotation(radToPointer, 60);
+      this.updateImage(Phaser.Math.radToDeg(radToPointer));
    }
    else {
       this.body.velocity.x = 0;
       this.body.velocity.y = 0;
+   }
+}
+
+Kungen.prototype.updateImage = function(deg) {
+
+   var imageIndex = -1;
+
+   if (deg < -45.0 && deg > -135.0) {
+      imageIndex = 0;
+   }
+   else if (deg < -135.0 || deg > 145.0) {
+      imageIndex = 1;
+   }
+   else if (deg < 145.0 && deg > 45.0) {
+      imageIndex = 2;
+   }
+   else {
+      imageIndex = 3;
+   }
+
+   if (imageIndex != this.currentImage) {
+      this.currentImage = imageIndex;
+      this.loadTexture(this.imageMap[this.currentImage]);
    }
 }
--- src/Preloader.js
+++ src/Preloader.js
@@ -16,7 +16,10 @@ Knugen.Preloader.prototype = {
 		this.load.image('background', 'assets/Background.png');
 		this.load.image('castle', 'assets/Castle.png');
 		this.load.image('gate', 'assets/Gate.png');
-		this.load.image('kungen', 'assets/Tmp_knugen.png');
+		this.load.image('kungen0', 'assets/Tmp_knugen_0.png');
+		this.load.image('kungen90', 'assets/Tmp_knugen_90.png');
+		this.load.image('kungen180', 'assets/Tmp_knugen_180.png');
+		this.load.image('kungen270', 'assets/Tmp_knugen_270.png');
 
 		// load spritesheets
 		//this.load.spritesheet('button-start', 'assets/button-start.png', 200, 100);
