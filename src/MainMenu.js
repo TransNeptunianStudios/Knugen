94f17381241f28920f65d8db9df6d97a7dd703dd
Reicher <robin.o.reicher@gmail.com>
2015-03-06 21:59:37 +0100

    added credits and version

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -5,11 +5,20 @@ KnugenGame.MainMenu.prototype = {
 	create: function(){
 		var background = this.add.sprite(0, 0, 'menuBackground');
 
-		var text = this.add.text(KnugenGame.WIDTH/2,
-									KnugenGame.HEIGHT-50,
-									'(Tryck för att bli Knugen)', // Va fan ska man skriva liksom?
-									{ font: "10px Arial"});
-		text.anchor.setTo(0.5);
+		// Credits
+		var text = "Programming: Robin Reicher & Mikael Larsson\nArt: David Levi";
+		var style = { font: "10px Arial", fill: "#000000", align: "center" };
+		var credits = this.game.add.text(KnugenGame.WIDTH/2, 10, text, style);
+		credits.anchor.setTo(0.5, 0);
+
+		var text = "(Tryck för att bli Knugen)";
+		var style = { font: "10px Arial", fill: "#000000", align: "center" };
+		var startText = this.game.add.text(KnugenGame.WIDTH/2, 200, text, style);
+		startText.anchor.setTo(0.5, 0);
+
+		var text = "Version: 0.1 Beta";
+		var style = { font: "10px Arial", fill: "#000000", align: "center" };
+		var version = this.game.add.text(5, 385, text, style);		
 
 		this.game.input.onDown.add(this.startGame, this);
 	},
--- src/Splash.js
+++ src/Splash.js
@@ -6,14 +6,23 @@ KnugenGame.Splash.prototype = {
       var logo = this.add.sprite(this.game.width/2, this.game.height/2, 'TNSlogo');
       logo.anchor.setTo(0.5);
       logo.scale.setTo(0.6);
-      var fadeTween = this.game.add.tween(logo).to({alpha: 0}, 1000, Phaser.Easing.Linear.None, false, 3000);
-      fadeTween.onComplete.add(this.startGame, this);
-      fadeTween.start();
+
 
       this.game.input.onDown.add(this.startGame, this);
 
       var music = this.game.add.audio('bgMusic', 0.7, true);
-      //music.play();
+      music.play();
+
+      // Music credits
+      var text = "Music:\nMinstrel Guild - Kevin MacLeod";
+      var style = { font: "10px Arial", fill: "#FFFFFF", align: "center" };
+      var credits = this.game.add.text(KnugenGame.WIDTH/2, 370, text, style);
+      credits.anchor.setTo(0.5, 0);
+
+      var logoFade = this.game.add.tween(logo).to({alpha: 0}, 1000, Phaser.Easing.Linear.None, true, 3000);
+      var creditFade = this.game.add.tween(credits).to({alpha: 0}, 1000, Phaser.Easing.Linear.None, false, 3000);
+      creditFade.onComplete.add(this.startGame, this);
+      creditFade.start();
    },
    startGame: function() {
       // start the Game state
