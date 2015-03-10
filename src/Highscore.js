2983d6c46f8094e2cd28f3b234456600cf3b62f0
Reicher <robin.o.reicher@gmail.com>
2015-03-10 19:20:01 +0000

    First entries from highscore shown

--- src/Highscore.js
+++ src/Highscore.js
@@ -1,25 +1,29 @@
 KnugenGame.Highscore = function(game){
-
 };
 KnugenGame.Highscore.prototype = {
    create: function(){
+
+      // Get our highscore
+      $.ajax({
+         context: this,
+         url: 'php/getHighscore.php',
+         type: 'GET',
+         success: this.gotHighScore
+      });
+
       this.add.sprite(0, 0, 'gameOverScreen');
       this.add.sprite(100, 270, 'KnugGroda');
 
       var graphics = this.game.add.graphics( 0, 0);
+      graphics.beginFill(0xFFFFFF, 1);
+      graphics.drawRoundedRect(10, 10, 220, 290, 10);
+      graphics.alpha = 0.5;
 
       // Header
       var text = "HighScore";
       var style = { font: "20px Arial", fill: "#000000", align: "center" };
       var header = this.game.add.text(KnugenGame.WIDTH/2, 10, text, style);
       header.anchor.setTo(0.5, 0);
-
-      graphics.beginFill(0xFFFFFF, 1);
-      graphics.drawRoundedRect(10, 10, 220, 290, 10);
-      graphics.alpha = 0.5;
-
-      // gör en get och läs in den json fil som skapas med highscore?
-
 
       // Score
       var text = "Score: " + this.game.points;
@@ -45,11 +49,27 @@ KnugenGame.Highscore.prototype = {
 
       this.game.input.onDown.add(this.startGame, this);
    },
+   gotHighScore: function(data){
+      var highscore = JSON.parse(data);
+
+      // Sort list
+      highscore.sort(function(a, b){
+         return b.score - a.score;
+      });
+
+      var style = { font: "10px Arial", fill: "#000000", align: "center" };
+      var y = 50
+      for(i = 0; i < highscore.length; ++i){
+         this.game.add.text(20, y, i+1, style);
+         this.game.add.text(80, y, highscore[i].nick, style);
+         this.game.add.text(200, y, highscore[i].score, style);
+         y += 20;
+      }
+   },
    compareScore: function(score){
       if (!this.supports_html5_storage()) { console.log("NOO"); return false; }
 
       var best = localStorage.getItem("BestLocal");
-      console.log("best: " + best);
       if(!best || best < score){
          localStorage["BestLocal"] = score;
          return score;
